94290123b02da27a8f878ec8c98b334a540d5f76
Kovalenko Alexander <avk013@live.ru>
2017-12-02 07:56:22 +0200

    получение ip и статистики

--- net_if_stat2mail/Form1.Designer.cs
+++ net_if_stat2mail/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- net_if_stat2mail/Form1.cs
+++ net_if_stat2mail/Form1.cs
@@ -4,8 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace net_if_stat2mail
@@ -15,6 +16,49 @@ namespace net_if_stat2mail
         public Form1()
         {
             InitializeComponent();
+        }
+        public static string GetLocalIpAddress()
+        {
+            foreach (var netI in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (netI.NetworkInterfaceType != NetworkInterfaceType.Wireless80211 &&
+                    (netI.NetworkInterfaceType != NetworkInterfaceType.Ethernet ||
+                     netI.OperationalStatus != OperationalStatus.Up)) continue;
+                foreach (var uniIpAddrInfo in netI.GetIPProperties().UnicastAddresses.Where(x => netI.GetIPProperties().GatewayAddresses.Count > 0))
+                {
+
+                    if (uniIpAddrInfo.Address.AddressFamily == AddressFamily.InterNetwork &&
+                        uniIpAddrInfo.AddressPreferredLifetime != uint.MaxValue)
+                        
+
+               return uniIpAddrInfo.Address.ToString(); }
+            }
+           // Logger.Log("You local IPv4 address couldn't be found...");
+            return null;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string p2="R", name="", rx_byte, tx_byte, rx_packet, tx_packet;
+            int p1=0;
+          //  label1.Text = GetLocalIpAddress();
+            foreach (var netI in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (netI.NetworkInterfaceType != NetworkInterfaceType.Wireless80211 &&
+                    (netI.NetworkInterfaceType != NetworkInterfaceType.Ethernet ||
+                     netI.OperationalStatus != OperationalStatus.Up)) continue;
+                foreach (var uniIpAddrInfo in netI.GetIPProperties().UnicastAddresses.Where(x => netI.GetIPProperties().GatewayAddresses.Count > 0))
+                {
+                 //   if (uniIpAddrInfo.Address.AddressFamily == AddressFamily.InterNetwork &&
+                 //       uniIpAddrInfo.AddressPreferredLifetime != uint.MaxValue)
+                                  
+                }
+            name = netI.Name;
+                    rx_byte = netI.GetIPv4Statistics().BytesSent.ToString();
+                    tx_byte = netI.GetIPv4Statistics().BytesReceived.ToString();
+                    rx_packet = netI.GetIPv4Statistics().UnicastPacketsSent.ToString();
+                    tx_packet = netI.GetIPv4Statistics().UnicastPacketsReceived.ToString();
+                    label2.Text += p1++.ToString()+"_"+name +"_"+ rx_byte + "_"+tx_byte+Environment.NewLine;
+            }
         }
     }
 }
