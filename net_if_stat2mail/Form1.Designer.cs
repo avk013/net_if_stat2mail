eebfd71f21449cd2361124f655446e891153e740
Kovalenko Alexander <avk013@live.ru>
2017-12-06 06:06:16 +0200

    b2+

--- net_if_stat2mail/Form1.Designer.cs
+++ net_if_stat2mail/Form1.Designer.cs
@@ -31,11 +31,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 206);
+            this.button1.Location = new System.Drawing.Point(3, 120);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(288, 37);
             this.button1.TabIndex = 0;
@@ -46,7 +47,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 53);
+            this.label1.Location = new System.Drawing.Point(12, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
@@ -60,6 +61,16 @@
             this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 1;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(288, 37);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -67,6 +78,7 @@
             this.ClientSize = new System.Drawing.Size(292, 273);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -80,6 +92,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- net_if_stat2mail/Form1.cs
+++ net_if_stat2mail/Form1.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
@@ -12,7 +13,7 @@ using System.Windows.Forms;
 namespace net_if_stat2mail
 {
     public partial class Form1 : Form
-    {
+    {public string path=@"e:\!\";
         public Form1()
         {
             InitializeComponent();
@@ -38,27 +39,108 @@ namespace net_if_stat2mail
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            string p2="R", name="", rx_byte, tx_byte, rx_packet, tx_packet;
+            string data = "", fn = "";
+            //формировать имя файла по типу
+            // имякомп_день-мес-год_час-мин-сек_время аптайма.txt
+            string p2 ="R", name="", rx_byte, tx_byte, rx_packet, tx_packet;
             int p1=0;
           //  label1.Text = GetLocalIpAddress();
-            foreach (var netI in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                if (netI.NetworkInterfaceType != NetworkInterfaceType.Wireless80211 &&
-                    (netI.NetworkInterfaceType != NetworkInterfaceType.Ethernet ||
-                     netI.OperationalStatus != OperationalStatus.Up)) continue;
+       foreach (var netI in NetworkInterface.GetAllNetworkInterfaces())
+         {if (netI.NetworkInterfaceType != NetworkInterfaceType.Wireless80211 &&
+           (//netI.NetworkInterfaceType != NetworkInterfaceType.Ethernet ||
+            netI.OperationalStatus != OperationalStatus.Up)) continue;
+            name = (netI.Name+"        ").Substring(0,8);
+            rx_byte = netI.GetIPv4Statistics().BytesSent.ToString();
+            tx_byte = netI.GetIPv4Statistics().BytesReceived.ToString();
+            rx_packet = netI.GetIPv4Statistics().UnicastPacketsSent.ToString();
+            tx_packet = netI.GetIPv4Statistics().UnicastPacketsReceived.ToString();
+                //
+                string ip_addr="";
                 foreach (var uniIpAddrInfo in netI.GetIPProperties().UnicastAddresses.Where(x => netI.GetIPProperties().GatewayAddresses.Count > 0))
+                {ip_addr+=uniIpAddrInfo.Address.ToString()+"_";}
+            
+            //
+            
+            label2.Text += 
+                    p1++.ToString()+"_"+name +"_"+ rx_byte + "_"+tx_byte+"_"+ip_addr+Environment.NewLine;
+            data+=
+                    p1++.ToString() + "_" + name + "_" + rx_byte + "_" + tx_byte + "_" + ip_addr + Environment.NewLine;
+            }
+            //скидываем в файл
+            string s = DateTime.Now.ToString("dd-MMMM-yyyy_HH-mm");
+            string[] m_ru = {"января", "февраля","марта", "апреля","мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+            string[] m_en = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" };
+            for (int i = 0; i < 12; i++) s=s.Replace(m_ru[i], m_en[i]);
+            //формируем имя файла
+            label1.Text = Environment.MachineName + "_" + s+ "_" + UpTime.ToString().Remove(UpTime.ToString().LastIndexOf("."));
+            fn = Environment.MachineName + "_" + s + "_" + UpTime.ToString().Remove(UpTime.ToString().LastIndexOf("."));
+            fn = fn.Replace(":", "_");
+            fn += ".txt";
+            File.WriteAllText(@path+@fn,data);
+            //отправляем файл на почту...
+            send_mail(@path + @fn);
+        }
+        public TimeSpan UpTime
+        {
+            get
+            {
+                using (var uptime = new PerformanceCounter("System", "System Up Time"))
                 {
-                 //   if (uniIpAddrInfo.Address.AddressFamily == AddressFamily.InterNetwork &&
-                 //       uniIpAddrInfo.AddressPreferredLifetime != uint.MaxValue)
-                                  
+                    uptime.NextValue();       //Call this an extra time before reading its value
+                    return TimeSpan.FromSeconds(uptime.NextValue());
                 }
-            name = netI.Name;
-                    rx_byte = netI.GetIPv4Statistics().BytesSent.ToString();
-                    tx_byte = netI.GetIPv4Statistics().BytesReceived.ToString();
-                    rx_packet = netI.GetIPv4Statistics().UnicastPacketsSent.ToString();
-                    tx_packet = netI.GetIPv4Statistics().UnicastPacketsReceived.ToString();
-                    label2.Text += p1++.ToString()+"_"+name +"_"+ rx_byte + "_"+tx_byte+Environment.NewLine;
             }
         }
+        public void send_mail(string attach)
+        {
+            if (File.Exists(@path + "net_if_stat2mail.cfg")){            
+        //Format net_if_stat2mail.cfg: servsmtp/n port/n login/n pass/n 2mail
+                string[] cfg = File.ReadAllLines(@path + "net_if_stat2mail.cfg");
+                if (cfg.Length == 5) {
+                    //smtp сервер
+                    string smtpHost = cfg[0];//"smtp.gmail.com";                         
+                    int smtpPort = Convert.ToInt16(cfg[1]);//587;//smtp порт                                        
+                    string login = cfg[2];//логин
+                    string pass = cfg[3];//пароль 
+                    SmtpClient client = new SmtpClient(smtpHost, smtpPort);//создаем подключение
+                    client.Credentials = new NetworkCredential(login, pass);
+                    client.EnableSsl = true;
+                    string from = cfg[2]; ;//От кого письмо
+                    string to = cfg[4]; ;//Кому письмо
+                    string subject = "Письмо от local"+ DateTime.Now.ToString("dd-MMMM-yyyy_HH-mm"); ;  
+                    string body = "";//Текст письма                   
+//Вложение для письма Если нужно не одно вложение, для каждого создаем отдельный Attachment
+                    Attachment attData = new Attachment(@attach);
+                    MailMessage mess = new MailMessage(from, to, subject, body);//Создаем сообщение  
+                    mess.Attachments.Add(attData);//прикрепляем вложение
+                    mess.SubjectEncoding = Encoding.UTF8;//прописываем заголовок 
+                    mess.BodyEncoding = Encoding.UTF8;
+                    //   mess.Headers["Content-type"] = "text/plain; charset=windows-1251";
+                    try { client.Send(mess); }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                } else MessageBox.Show("Read stricture config file_"+ cfg.Length.ToString());
+            } else MessageBox.Show("Error read config file");
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string data = "", fn = "";
+            //скидываем в файл
+            string s = DateTime.Now.ToString("dd-MMMM-yyyy_HH-mm");
+            string[] m_ru = { "января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря" };
+            string[] m_en = { "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec" };
+            for (int i = 0; i < 12; i++) s = s.Replace(m_ru[i], m_en[i]);
+            //формируем имя файла
+            label1.Text = Environment.MachineName + "_" + s + "_" + UpTime.ToString().Remove(UpTime.ToString().LastIndexOf("."));
+            fn = Environment.MachineName + "_" + s + "_" + UpTime.ToString().Remove(UpTime.ToString().LastIndexOf("."));
+            fn = fn.Replace(":", "_");
+            fn += ".txt";
+            File.WriteAllText(@path + @fn, data);
+        }
     }
+ 
 }
